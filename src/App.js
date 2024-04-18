0794b36d2936cbd4761aa339b20d2cd2f7de8cfd
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-04-18 22:19:09 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import NewsImage from "./news.jpg";
+import "./App.css";
+import axios from "axios";
+import data from "./data";
 function App() {
+  const [news, setNews] = useState({
+    category: false,
+    country: false,
+    NewsChange: false,
+  });
+  const fetch = async () => {
+    console.log("how many");
+    let res = await axios.get(
+      "https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json"
+    );
+    setNews((pre) => ({
+      ...pre,
+      total: res.data.totalResults,
+      news: res.data.articles,
+    }));
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div>
+          <p
+            onClick={() =>
+              setNews((pre) => ({
+                ...pre,
+                country: !news.country,
+                category: false,
+              }))
+            }
+          >
+            Country
+          </p>
+          <p
+            onClick={() =>
+              setNews((pre) => ({
+                ...pre,
+                category: !news.category,
+                country: false,
+              }))
+            }
+          >
+            Category
+          </p>
+        </div>
+        <div>
+          <img
+            onClick={() =>
+              setNews((pre) => ({ ...pre, NewsChange: !news.NewsChange }))
+            }
+            src="https://cdn-icons-png.flaticon.com/128/2540/2540832.png"
+            alt="News_icon"
+          />
+        </div>
       </header>
-    </div>
+      <div
+        className={
+          news.category ? "category_container" : "category_container hide"
+        }
+      >
+        {data.category.map((item) => (
+          <p key={item.name}>{item.name}</p>
+        ))}
+      </div>
+      <div
+        className={
+          news.country ? "country_container" : "country_container hide"
+        }
+      >
+        {data.Country.map((item, index) => (
+          <div className="country" key={`parent_${index}`}>
+            <img src={item.url} alt={item.name} key={`${item.name} flag`} />
+            <p>{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <div id={news.NewsChange ? "News_Channels" : "News_channels_Hide"}>
+        <div id="BBC_News">
+          <img
+            src="https://download.logo.wine/logo/BBC_News/BBC_News-Logo.wine.png"
+            alt="BBC_NEWS_icon"
+          />
+          <p>BBC News</p>
+        </div>
+        <div id="CNN_News">
+          <img
+            src="https://download.logo.wine/logo/CNN/CNN-Logo.wine.png"
+            alt="CNN_NEWS_icon"
+          />
+          <p>CNN News</p>
+        </div>
+        <div id="FOX_News">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2020/11/Fox-News-Channel-Emblem.png"
+            alt="FOX_NEWS_icon"
+          />
+          <p>FOX News</p>
+        </div>
+        <div id="GOOGLE_News">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/2702/2702605.png"
+            alt="GOOGLE_NEWS_icon"
+          />
+          <p>GOOGLE </p>
+        </div>
+      </div>
+      <div className="Home">
+        {news?.news?.map((item, index) => (
+          <div id="News" key={`News${index + 1}`}>
+            <img
+              src={
+                item.urlToImage === " " || item.urlToImage === undefined
+                  ? NewsImage
+                  : item.urlToImage
+              }
+              alt="Image"
+              key={item.title}
+            />
+            <div id="content_container">
+              <h1 key={`heading ${index}`}>{item.title}</h1>
+              <p key={`content ${index}`}>{item.description}</p>
+              <span>{`${item.publishedAt.split("T")[0]} / ${
+                item.publishedAt.split("T").pop().split("Z")[0]
+              }`}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const category = [
+  {
+    name: "Business",
+    code: "business",
+  },
+  {
+    name: "Entertainment",
+    code: "entertainment",
+  },
+  {
+    name: "General",
+    code: "general",
+  },
+  {
+    name: "Health",
+    code: "health",
+  },
+  {
+    name: "Science",
+    code: "science",
+  },
+  {
+    name: "Sports",
+    code: "sports",
+  },
+  {
+    name: "Technology",
+    code: "technology",
+  },
+];
+const Country = [
+  {
+    name: "India",
+    code: "in",
+    url: "https://cdn-icons-png.flaticon.com/128/3909/3909444.png",
+  },
+  {
+    name: "USA",
+    code: "us",
+    url: "https://cdn-icons-png.flaticon.com/128/3909/3909383.png",
+  },
+  {
+    name: "Australia",
+    code: "au",
+    url: "https://cdn-icons-png.flaticon.com/128/3909/3909435.png",
+  },
+  {
+    name: "Russia",
+    code: "ru",
+    url: "https://cdn-icons-png.flaticon.com/128/9994/9994030.png",
+  },
+  {
+    name: "France",
+    code: "fr",
+    url: "https://cdn-icons-png.flaticon.com/128/9993/9993540.png",
+  },
+  {
+    name: "United Kingdom",
+    code: "gb",
+    url: "https://cdn-icons-png.flaticon.com/128/3909/3909136.png",
+  },
+];
+const data = { category, Country };
+export default data;
